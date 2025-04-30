fdb8ebeae26318ce3656a46d7e5d832099d51fb4
Alexandre Paes Rigão <rigao.alexandre@gmail.com>
2025-04-30 00:10:31 -0300

    feat: add report download

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, StreamableFile } from '@nestjs/common';
 import { ReportService } from './report.service';
+import { BooksService } from 'src/books/books.service';
+import { stringify } from 'csv-stringify';
 
 @Controller('report')
 export class ReportController {
-  constructor(private readonly reportService: ReportService) {}
+  constructor(
+    private readonly reportService: ReportService,
+    private readonly booksService: BooksService,
+  ) {}
 
-  @Get()
-  index() {
+  @Get('/')
+  async index() {
     return this.reportService.index();
+  }
+
+  @Get('/books')
+  async books() {
+    const books = await this.booksService.findAll();
+
+    const header = [
+      'id',
+      'title',
+      'author',
+      'isbn',
+      'description',
+      'status',
+      'memberId',
+      'dueDate',
+      'createdAt',
+      'updatedAt',
+    ];
+
+    const stream = stringify();
+
+    stream.write(header);
+
+    books.forEach((item) => stream.write(Object.values(item.toJSON())));
+
+    stream.end();
+
+    return new StreamableFile(stream, {
+      type: 'text/csv',
+      disposition: `attachment; filename=books-${new Date().toISOString()}.csv`,
+    });
   }
 }
